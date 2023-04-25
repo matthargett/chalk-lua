a189f76faf87228b72c646997703deb55597ef80
Matt Hargett <plaztiksyke@gmail.com>
2023-04-25 12:21:56 -0700

    Update source/index.lua
    Co-authored-by: jeparlefrancais <35781636+jeparlefrancais@users.noreply.github.com>

--- source/index.lua
+++ source/index.lua
@@ -89,7 +89,7 @@ end
 
 styles.visible = (function()
 	local this = styles.visible
-	createBuilder(this, this._styler, true)
+	return createBuilder(this, this._styler, true)
 end)()
 
 local usedModels = { "rgb", "hex", "keyword", "hsl", "hsv", "hwb", "ansi", "ansi256" }
